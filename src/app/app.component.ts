8b0355928c381637f62871f4abeb4a2bb5064ab4
Morozowi Morozow <Bugonort1@gmail.com>
2020-09-15 15:26:26 +0300

    video not done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { MediaObserver, MediaChange } from '@angular/flex-layout';
+// import { from, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // export class AppComponent implements OnInit, OnDestroy {
   title = 'AngularTest';
+  // mediaSub: Subscription;
+  // deviseXs: boolean;
+  // constructor(public MediaObserver: MediaObserver) {}
+
+  // ngOnInit() {
+  //   this.mediaSub = this.MediaObserver.media$.subscribe(
+  //     (result: MediaChange) => {
+  //       console.log(result.mqAlias);
+  //       this.deviseXs = result.mqAlias === 'xs' ? true : false;
+  //     }
+  //   );
+  // }
+  // ngOnDestroy() {
+  //   this.mediaSub.unsubscribe();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MainComponent, HeaderComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
